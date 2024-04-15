7b6ff8e042971c54d4df291e780905499c37985f
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-15 18:29:35 +0200

    Destroy

---
+++ src/main/java/controlador/Destroy.java
@@ -0,0 +1,44 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.ModeloCamionero;
+
+/**
+ * Servlet implementation class Destroy
+ */
+@WebServlet("/Destroy")
+public class Destroy extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Destroy() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ModeloCamionero cm = new ModeloCamionero();
+		cm.delete(request.getParameter("dni"));
+		response.sendRedirect("Index");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
