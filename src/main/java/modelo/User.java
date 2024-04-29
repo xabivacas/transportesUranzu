be3c1ca881f3fc1c5a96dfcd7808d6e715b7414e
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-29 14:41:48 +0200

    Login y dejar un poco bonito el nav

---
+++ src/main/java/modelo/User.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class User {
+
+	private String nombre;
+	private String contrasenia;
+	private String tipo;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
